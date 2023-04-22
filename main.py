c51a7db06a6a5d67e4cd0387060e26fe40740747
JolyStin <renatnetesa@gmail.com>
2023-04-22 10:07:54 +0200

    add category_expenses function

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+def category_expenses(file_name):
+    total_by_category = {}
+
+    with open(file_name, "r", encoding="utf-8") as file:
+        for line in file:
+            *_, price, category = line.split()
+            price = float(price.replace("$", "").strip())
+            if category in total_by_category:
+                total_by_category[category] += price
+            else:
+                total_by_category[category] = price
+    return total_by_category
+
+
 def main():
     ...
 
