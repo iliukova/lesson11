415e78a4383121e35ea98767cda93f83bf5f79c6
Inna <innailiukova@gmail.com>
2023-04-22 10:54:03 +0300

    add total_expenses_function

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+def total_expenses(file_name):
+    s = 0
+    with open(file_name, "r", encoding="utf-8") as file:
+        for line in file:
+            _, *name, price, _ = line.split()
+            s += float(price.replace("$", "").strip())
+    return s
+
+
 def main():
     ...
 
